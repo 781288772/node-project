a0fde069858d645adc01f0a3f34cebf1651f6908
chy <781288772@qq.com>
2021-12-08 19:54:23 +0800

    添加用户表

---
+++ sql/test_sys_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO test.sys_user (id, username, phone, email, password) VALUES (1, '张三', '18718099978', '781288772@qq.com', '123');
+INSERT INTO test.sys_user (id, username, phone, email, password) VALUES (2, '李四', '18718099978', '781288772@qq.com', '123');
+INSERT INTO test.sys_user (id, username, phone, email, password) VALUES (28, 'admin', null, null, '123456');

4c1be77f279b6557606ab3bbfe4d12e385e856ec
chy <781288772@qq.com>
2021-12-08 19:59:50 +0800

    添加表

---
+++ sql/script.sql
@@ -0,0 +1,11 @@
+create table sys_user
+(
+    id       int(255) auto_increment
+        primary key,
+    username varchar(255) null,
+    phone    varchar(255) null,
+    email    varchar(255) null,
+    password varchar(255) null
+);
+
+
